9180fb39e0bbb1a148be6b8391c92b3b345e018b
Gundega Dekena <gundega@spacetree.cloud>
2020-01-08 08:26:28 +0200

    pievienota health check route

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ app = Flask('app')
 @app.route('/')
 def index_page():
   return render_template('index.html')
+
+
+@app.route('/health')
+def health_check():
+  return "OK"
+
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
