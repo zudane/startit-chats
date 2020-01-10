60c6b68c56863ae04d4c5103c2ea00e15c7e7da4
zudane <zudane@inbox.lv>
2020-01-10 12:25:53 +0200

    Mainits main

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 from flask import Flask, render_template
-
-
 app = Flask('app')
-
-
 @app.route('/')
 def index_page():
-  return render_template('index.html')
-
-@app.rout('/health')
-def health_check();
-  return "Ok"
+  return render_template("index.html")
+@app.route('/helth')
+def helth():
+  return "OK"
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000, debug=True)
