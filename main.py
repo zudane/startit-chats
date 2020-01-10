5c6515a28456f370a09f7674955f7b5ca6a88c46
zudane <zudane@inbox.lv>
2020-01-10 12:17:07 +0200

    Favicon

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = Flask('app')
 
 @app.route('/')
 def index_page():
-  return "Tagad sveicu es!!!!"
+  return render_template('index.html')
 
-
+@app.rout(health')
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000, debug=True)
