fd6387c1bd23346495ff24ac378eeb93fa26b4bc
zudane <zudane@inbox.lv>
2020-01-10 12:19:48 +0200

    Favicon

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ app = Flask('app')
 def index_page():
   return render_template('index.html')
 
-@app.rout(health')
+@app.rout('/health')
+def health_check();
+  return "Ok"
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000, debug=True)
