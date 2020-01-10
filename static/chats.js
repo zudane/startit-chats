ed18f4ebbbe95e7e90f6c40a8a2c42eda6806e54
Gundega Dekena <gundega@spacetree.cloud>
2020-01-10 03:53:56 +0200

    0.4 posma risinājuma kods

--- static/chats.js
+++ static/chats.js
@@ -1,5 +1,5 @@
 const ATJAUNOT = 50000;
-
+const VERSIJA = "0.4"
 
 /*
 Ielādē tērzēšanas datus no servera
@@ -8,7 +8,7 @@ Uzstāda laiku pēc kāda atkārtoti izsaukt šo pašu funkciju
 async function lasiChatu() {
     const atbilde = await fetch('/chats/lasi');
     const datuObjekts = await atbilde.json();
-    raadiChatuVienkarsi(datuObjekts);
+    raadiChataRindas(datuObjekts);
     await new Promise(resolve => setTimeout(resolve, ATJAUNOT));
     await lasiChatu();
 }
@@ -25,31 +25,68 @@ function raadiChatuVienkarsi(dati) {
 }
 
 
+function raadiChataRindas(dati) {
+    const chatUL = document.getElementById("chats");
+    // novaacam ieprieksheejo saturu
+    while (chatUL.firstChild) {
+        chatUL.firstChild.remove();
+    }
+    for (let rinda of dati["chats"]) {
+      chatLI = izveidoJaunuRindu(rinda);
+      chatUL.appendChild(chatLI);
+    }
+    // noskrolleejam uz leju pie peedeejaa chata texta
+    var chatScrollBox = chatUL.parentNode;
+    chatScrollBox.scrollTop = chatScrollBox.scrollHeight;
+  }
+  
+  
+  function izveidoJaunuRindu(zinja) { 
+    let newLI = document.createElement("li");
+    newLI.className = "left clearfix"
+    let newDiv = document.createElement("div"); 
+    newDiv.className = "chat-body clearfix"
+    let newContent = document.createTextNode(zinja); 
+    newLI.appendChild(newDiv); 
+    newDiv.appendChild(newContent); 
+    return newLI;
+  }
+
+
 /*
 Publicē tērzēšanas ziņas datus uz serveri
 */
 async function suutiZinju() {
     // Nolasa ievades lauka saturu
     let zinjasElements = document.getElementById("zinja");
-    let zinja = zinjasElements.value;
-    // izdzēš ievades lauku
-    zinjasElements.value = "";
+    const zinja = zinjasElements.value;
 
-    const atbilde = await fetch('/chats/suuti', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ "chats": zinja })
-    });
-    const datuObjekts = await atbilde.json();
+    // pārbaudām vai ir vispār kaut kas ierakstīts
+    if (zinja.length > 0) {
 
-    // parāda jauno chata saturu
-    raadiChatuVienkarsi(datuObjekts);
+        // izdzēš ievades lauku
+        zinjasElements.value = "";
+
+        const atbilde = await fetch('/chats/suuti', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ "chats": zinja })
+        });
+        const datuObjekts = await atbilde.json();
+        // parāda jauno chata saturu
+        raadiChataRindas(datuObjekts);
+    } else {
+        console.log("Tukšu ziņu nesūtām uz serveri")
+    }
+
 }
 
 
 // Ērtības funkcionalitāte
+var versijasLauks = document.getElementById("versija");
+versijasLauks.innerHTML = "JS versija: " + VERSIJA;
 // Atrod ievades lauku
 var ievadesLauks = document.getElementById("zinja");
 // Gaida signālu no klaviatūras, ka ir nospiests Enter taustiņš
