8dbbe11003c37a12f537a4979820015ddb6ada3c
Gundega Dekena <gundega@spacetree.cloud>
2020-01-10 02:50:09 +0200

    nomainits template uz chats.html

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask('app')
 
 @app.route('/')
 def index_lapa():
-  return render_template('index.html')
+  return render_template('chats.html')
 
 
 @app.route('/health')
@@ -24,11 +24,10 @@ def ielasit_chatu():
 
 @app.route('/chats/suuti', methods=['POST'])
 def suutiit_zinju():
-  dati = request.data
-  json_dati = json.loads(dati)
+  dati = request.json
   
   with open("chats.txt", "a", newline="") as f:
-    f.write(json_dati["chats"] + "\n")
+    f.write(dati["chats"] + "\n")
 
   return ielasit_chatu()
   
